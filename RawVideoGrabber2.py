093fec0de94c5d1a8f2bec0bdb567c21fa52c74d
bruand81 <andbruno@unisa.it>
2018-12-13 13:36:13 +0100

    Cattura dei frame con il modulo picamera

--- RawVideoGrabber2.py
+++ RawVideoGrabber2.py
@@ -1,10 +1,11 @@
 import cv2
 import os
 import math
-from time import time
+import time
 import _thread
 import configparser
 import shutil
+import picamera
 
 
 class RawVideoGrabber:
@@ -18,52 +19,53 @@ class RawVideoGrabber:
         # Grab as a sequence of frames
         print("Camera = %s" % opts["CameraIndex"])
         print("Use threads = %s" % opts["UseThread"])
+        dest_dir = self.__manage_path__(opts)
+        print("Frames will be stored in %s" % os.path.abspath(dest_dir))
+        self.w = opts["MaxWidth"]
+        self.h = opts["MaxHeight"]
+
+        if opts["OutputFormat"] == 'video':
+            return self.grab_as_video(dest_dir=dest_dir, opts=opts)
+        elif opts["OutputFormat"] == 'image':
+            return self.grab_as_image(dest_dir=dest_dir, opts=opts)
+        else:
+            raise ValueError("Value \"%s\" not recognized for OutputFormat\n\t Admitted value are: video or image")
+
+    def grab_as_video(self, dest_dir, opts):
+        video_format = opts["VideoFormat"]
+        # video_output = os.path.join(dest_dir, opts["OutputVideoFilename"])
+        image_output_template = os.path.join(dest_dir, opts["OutputImageFilename"])
+        self.fps = opts["MaxFramerate"]
+        self.numFrames = self.fps * opts["NumSecs"]
+        self.numDigits = math.ceil(math.log10(self.numFrames))
+        # frame_num_expansion = "%s"
+
+        with picamera.PiCamera(camera_num=opts["CameraIndex"]) as camera:
+            camera.resolution = (self.w, self.h)
+            camera.start_preview()
+            time.sleep(2)
+            # camera.capture(video_output, video_format)
+            start_time = time.time()
+            j = 0
+            try:
+                for i, filename in enumerate(camera.capture_continuous(image_output_template,format=video_format)):
+                    j=i
+                    if i == self.numFrames:
+                        break
+            finally:
+                end_time = time.time()
+                time_taken = end_time - start_time
+                print("Saved %d frames in %d seconds" % (j, time_taken))
+                print("Effective average fps: %d" % (j / time_taken))
+
+        return
+
+    def grab_as_image(self, dest_dir, opts):
+        # Grab as a sequence of frames from camera as a sequence of images
         cap = self.__open_cap__(opts)
         print("Width: %d, height: %d" % (self.w, self.h))
         print("FrameRate: %d fps" % self.fps)
         print("Total number of frames to be captured: %d" % self.numFrames)
-
-        dest_dir = self.__manage_path__(opts)
-        print("Frames will be stored in %s" % os.path.abspath(dest_dir))
-
-        if opts["OutputFormat"] == 'video':
-            return self.grab_as_video(cap, dest_dir, opts)
-        elif opts["OutputFormat"] == 'image':
-            return self.grab_as_image(cap, dest_dir, opts)
-        else:
-            raise ValueError("Value \"%s\" not recognized for OutputFormat\n\t Admitted value are: video or image")
-
-    def grab_as_video(self, cap, dest_dir, opts):
-        # Grab as a sequence of frames from camera as a video file
-        fourcc_format = opts["VideoFormat"]
-        video_output = os.path.join(dest_dir, opts["OutputVideoFilename"])
-
-        out = cv2.VideoWriter(os.path.join(dest_dir, video_output),
-                              cv2.VideoWriter_fourcc(fourcc_format[0], fourcc_format[1], fourcc_format[2],
-                                                     fourcc_format[3]), self.fps, (self.w, self.h))
-
-        print("Capturing frames...")
-        i = 0
-        start_time = time()
-        while cap.isOpened():
-            ret, frame = cap.read()
-            if ret:
-                out.write(frame)
-
-            i += 1
-
-            if i >= self.numFrames:
-                break
-
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                break
-        end_time = time()
-        time_taken = end_time - start_time
-        print("Saved %d frames in %d seconds" % (self.numFrames, time_taken))
-        return
-
-    def grab_as_image(self, cap, dest_dir, opts):
-        # Grab as a sequence of frames from camera as a sequence of images
 
         image_output_template = opts["OutputImageFilename"]
         #frame_num_expansion = "%0"+str(self.numDigits)+"d"
@@ -71,11 +73,11 @@ class RawVideoGrabber:
         i = 0
 
         print("Capturing frames...")
-        start_time = time()
+        start_time = time.time()
         while cap.isOpened():
             ret, frame = cap.read()
             if ret:
-                fname = image_output_template.format(frame_num_expansion % int(round(time() * 1000)))
+                fname = image_output_template.format(frame_num_expansion % int(round(time.time() * 1000)))
                 if(opts["UseThread"]):
                     try:
                         _thread.start_new_thread(RawVideoGrabber.__store_image_file__, (frame, fname, dest_dir, ))
@@ -92,7 +94,7 @@ class RawVideoGrabber:
 
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
-        end_time = time()
+        end_time = time.time()
         time_taken = end_time - start_time
         print("Saved %d frames in %d seconds" % (self.numFrames, time_taken))
         print("Effective average fps: %d" % (self.numFrames/time_taken))
@@ -131,7 +133,7 @@ if __name__ == '__main__':
     # Reading configuration from file
     options = dict()
     config = configparser.ConfigParser()
-    config.read('RawVideoGrabber.ini')
+    config.read('RawVideoGrabber2.ini')
     options["CameraIndex"] = int(config['DEFAULT']['CameraIndex'])
     options["MaxWidth"] = int(config['DEFAULT']['MaxWidth'])
     options["MaxHeight"] = int(config['DEFAULT']['MaxHeight'])
